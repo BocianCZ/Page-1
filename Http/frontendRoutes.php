cf108538dabe259d11bd1b26b45126d93c9d472b
David Cap <david.cap@bocians.net>
2017-07-26 17:49:51 -0700

    disable homepage routing

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -4,11 +4,11 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 if (! App::runningInConsole()) {
-    $router->get('/', [
-        'uses' => 'PublicController@homepage',
-        'as' => 'homepage',
-        'middleware' => config('asgard.page.config.middleware'),
-    ]);
+//    $router->get('/', [
+//        'uses' => 'PublicController@homepage',
+//        'as' => 'homepage',
+//        'middleware' => config('asgard.page.config.middleware'),
+//    ]);
     $router->any('{uri}', [
         'uses' => 'PublicController@uri',
         'as' => 'page',
